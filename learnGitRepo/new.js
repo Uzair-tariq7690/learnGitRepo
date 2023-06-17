b9e689e74e84cc83c4955fc548e5396f3b5e900a
Uzair Tariq <javascriptbyuzair@gmail.com>
2023-06-17 13:19:41 +0500

    real charice

--- learnGitRepo/new.js
+++ learnGitRepo/new.js
@@ -3,3 +3,4 @@ console.log('master branch');
 console.log('real branch')
 console.log('real branch updated')
 console.log('real branch updated tharice ')
+console.log('real branch updated charice ')
